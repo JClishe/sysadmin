e847e03b8e1bdbebaad414897f9d98983cfa4dd3
Jason Clishe <clishej@gmail.com>
2022-11-25 11:51:01 -0500

    commit

---
+++ defaults/prefs.sh
@@ -0,0 +1,115 @@
+# System Preferences
+# List all system preferences manually modified during setup, and try to find the appropriate programmatic command
+
+BATTERY
+Low power mode on battery 
+Prevent Mac from sleeping when the display is off 
+Turn display off after 5 minutes on battery
+Turn display off after 1 hour on power 
+
+DOCK & MENU BAR 
+Get size of current Dock, create command to set 
+Get magnification of current Dock, create command to set 
+Minimize windows using Genie
+Minimize windows into application icon
+Animate opening applications 
+Automatically hide and show the Dock 
+Show indicators for open applications 
+Do not show recent applications in Dock 
+
+GENERAL 
+Dark mode 
+Close windows when quitting an app 
+Recent items: None 
+Allow Handoff between this Mac and iCloud devices 
+
+KEYBOARD
+Text repeat - get current; create command to set 
+Delay until repeat - get current; create command to set 
+Disable correct spelling automatically 
+
+LANGUAGE & REGION
+General | Advanced: First day of week: Monday 
+
+MISSION CONTROL
+Do not automatically rearrange Spaces based on most recent use 
+When switching to an app, switch to a Space with open app windows
+Do not group windows by application 
+Displays have seperate Spaces 
+
+Hot Corners
+Upper left: Application windows
+Lower left: Desktop
+Upper right: None
+Lower right: Quick Note 
+
+MOUSE 
+Secondary click - click on right side 
+Tracking speed - get current and set 
+
+NOTIFICATIONS & FOCUS
+
+
+
+PASSWORDS
+
+
+
+PRINTERS & SCANNERS
+
+
+
+SCREEN TIME 
+
+
+
+SECURITY & PRIVACY 
+
+
+
+SHARING
+
+
+
+SIRI 
+
+
+
+SOFTWARE UPDATE 
+
+
+
+SOUND 
+
+
+
+SPOTLIGHT 
+
+
+
+STARTUP DISK 
+
+
+
+TIME MACHINE 
+
+
+
+TOUCH ID 
+
+
+
+TRACKPAD 
+
+
+
+USERS & GROUPS 
+
+
+
+WALLET & APPLE PAY 
+
+
+
+
+# General Preferences
